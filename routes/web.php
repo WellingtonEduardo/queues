09f8b34c6aca578f34e970058fec520e014f2604
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-12-11 16:15:06 -0300

    I created the PullRequest model migration and implemented the creation, I also placed the laravel debugbar in the project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Models\PullRequest;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $pullRequests = Http::get('https://api.github.com/repos/laravel/laravel/pulls?state=all');
+
+    foreach ($pullRequests->json() as $pullRequest) {
+        PullRequest::create(
+            [
+                'api_id' => $pullRequest['id'],
+                'api_number' => $pullRequest['number'],
+                'state' => $pullRequest['state'],
+                'title' => $pullRequest['title'],
+                'api_created_at' => $pullRequest['created_at'],
+                'api_updated_at' => $pullRequest['updated_at'],
+                'api_closed_at' => $pullRequest['closed_at'] ?? null,
+                'api_merged_at' => $pullRequest['merged_at'] ?? null,
+            ]
+        );
+
+    }
+
+
 });
